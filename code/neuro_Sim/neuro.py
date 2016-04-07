fixed van Rossum computation; claening up

code/neuro_Sim/neuro.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 import scipy as scp
 import matplotlib as mp
-from pylab import *
+#from pylab import *
 import matplotlib.pyplot as plt
 
 # ==============================================================================
@@ -66,9 +66,9 @@ class synapse:
     self.G_s_23     = 0
     self.G_s_4      = 0
     for i in range(len(self.preSynGs)):
-      self.G_s_1  += 0.5*self.preSynGs[i]*exp(- self.preSynTs[i] / self.t_s)
-      self.G_s_23 += 0.5*self.preSynGs[i]*exp(-(self.preSynTs[i] + 0.25*self.dt)/self.t_s)
-      self.G_s_4  += 0.5*self.preSynGs[i]*exp(-(self.preSynTs[i] + self.dt)/self.t_s)
+      self.G_s_1  += 0.5*self.preSynGs[i]*math.exp(- self.preSynTs[i] / self.t_s)
+      self.G_s_23 += 0.5*self.preSynGs[i]*math.exp(-(self.preSynTs[i] + 0.25*self.dt)/self.t_s)
+      self.G_s_4  += 0.5*self.preSynGs[i]*math.exp(-(self.preSynTs[i] + self.dt)/self.t_s)
     return self.G_s_1
 
 
@@ -257,6 +257,25 @@ def getPoissonTrain(T, dt, sRate) :
 # ------------------------------------------------------------------------------
 # Functions implementing van Rossum metric
 # ------------------------------------------------------------------------------
+def vR_computeDistance(u, v, tau) :
+  dist = 0
+  for i in range(len(u)):
+    for j in range(len(u)):
+      dist +=   math.exp(- abs(u[i] - u[j])/tau)
+      
+  for i in range(len(v)):
+    for j in range(len(v)):
+      dist +=   math.exp(- abs(v[i] - v[j])/tau)
+      
+  for i in range(len(u)):
+    for j in range(len(v)):
+      dist -= 2*math.exp(- abs(u[i] - v[j])/tau)
+      
+  dist = math.sqrt(dist)
+  
+  return dist
+
+
 # filter spike train through exponential kernel to translate in vR form---------
 def vR_mapTrain(spikes, dt, T, tau, mu) :
   time = np.arange(0, T, dt)
@@ -265,24 +284,24 @@ def vR_mapTrain(spikes, dt, T, tau, mu) :
   for i, t_i in enumerate(time) :
     for j in range(len(spikes)) :
       t = t_i - spikes[j]
-      h[i] += (1-mu)*tau*exp(-t/tau) if t >= 0 else 0
+      h[i] += (1-mu)*math.exp(-t/tau) if t >= 0 else 0
       
   return h 
 
 # calculate distance between spike trains in van Rossum form--------------------
-def vR_computeDistance(f1, f2, dt) :
+def vR_computeDistance_mappedTrains(f1, f2, dt) :
   diff = np.zeros(len(f1))
   dist = 0
   for i in range(len(f1)) :
     diff[i] = f1[i] - f2[i]  
     dist += diff[i]*diff[i]*dt
     
-  dist = sqrt(dist)
+  dist = math.sqrt(dist)
 
   return dist, diff
+  
+  
 
-  
-  
 # ------------------------------------------------------------------------------
 # Function implementing Victor-Purpura spike-time metric
 # ------------------------------------------------------------------------------    
@@ -307,7 +326,6 @@ def VP_computeDistance(t1, t2, q) :
 
 
 
-
 # ------------------------------------------------------------------------------
 # objects to store the settings for a metric
 # ------------------------------------------------------------------------------
@@ -316,14 +334,19 @@ class metric(object):
     self.type = name
     
     if name == 'vR':
+      self.tau = tau
+     
+    elif name == 'VP':
+      self.q  = q
+
+#OLD INEFFICIENT WAY      
+'''    
+    if name == 'vR_old':
       self.dt  = dt
       self.T   = T
       self.tau = tau
       self.mu  = mu
-      
-    elif name == 'VP':
-      self.q  = q
-
+''' 
 
 
 
@@ -333,8 +356,27 @@ class metric(object):
 def getDistanceMap(trains, metric):
   N = len(trains)
   dMap = np.zeros([N,N])
+
+  for i in range(N):
+    for j in range(N):
+      if dMap[j][i] != 0 :
+        dMap[i][j] = dMap[j][i]
+      elif i != j :      
+        if metric.type == 'vR':
+          dMap[i][j] = vR_computeDistance(trains[i], trains[j], metric.tau)
+        elif metric.type == 'VP':
+          dMap[i][j] = VP_computeDistance(trains[i], trains[j], metric.q)
   
-  if metric.type == 'vR':  
+  return dMap
+
+#OLD VERISON WITH INEFFICIENT VAN ROSSUM FIRST MAPPING TO NUMERICALLY 
+#APPRXIMATED FUNCTION AND THEN DOING EUCLIDEAN DISTANCE
+'''
+def getDistanceMap(trains, metric):
+  N = len(trains)
+  dMap = np.zeros([N,N])
+  
+  if metric.type == 'vR_old':  
     vR_trains = []
     for t in range(N):
       vR_trains += [vR_mapTrain(trains[t], metric.dt, metric.T, metric.tau, metric.mu)]
@@ -345,11 +387,12 @@ def getDistanceMap(trains, metric):
         dMap[i][j] = dMap[j][i]
       elif i != j :      
         if metric.type == 'vR':
-          dMap[i][j], dif = vR_computeDistance(vR_trains[i], vR_trains[j], metric.dt)
+          dMap[i][j], dif = vR_computeDistance_mappedTrains(vR_trains[i], vR_trains[j], metric.dt)
         elif metric.type == 'VP':
           dMap[i][j] = VP_computeDistance(trains[i], trains[j], metric.q)
   
   return dMap
+'''
 
   
   

code/neuro_Sim/neuroSim2.py
@@ -1,13 +1,28 @@
 from __future__ import division
+
+from neuro import *
+
 import numpy as np
 import scipy as scp
 import matplotlib as mp
 import matplotlib.pyplot as plt
 
-from pylab import *
-from neuro import *
-
 plt.switch_backend('QT4Agg')
+
+# EXPERIMENTAL NETWORK CONNECTIVITY
+#
+# Integrate-and-fre neurons {0,1}
+#
+#       0     1     
+#       ^     ^
+#       |\1-g/|
+#       | \ / |
+#     g |  x  | g        Synapse strength g in [0,1]
+#       | / \ |
+#       |/   \|
+#       2     3
+#        
+# Poisson neurons {2,3}
     
 # Global parameters ------------------------------------------------------------
 mili  = 0.001          # Scaling factor 10^-3
@@ -43,10 +58,11 @@ g_bin = 1.0/g_res      # width of synapse strength step based on g_res
 h1 = 10                # size of open balls in spike-trrain metric space
 h2 = 10
 
-tau_vR = 1
-vR_metric = metric('vR', dt=dt, T=T, tau=tau_vR, mu=0)
+tau_vR = 12*mili
+vR_metric = metric('vR', tau = tau_vR)
 VP_metric = metric('VP', q=200)
 
+#metric = vR_metric
 metric = VP_metric
 
 MI_2_0 = []
@@ -56,7 +72,7 @@ MI_2_1 = []
 MI_2_1_Sum = 0
 
 
-# Experiments Simulation--------------------------------------------------------
+# Experiment Simulation---------------------------------------------------------
 
 # Convey experiment over a variation of synapse strengths
 for g in range(1, g_res) :
@@ -120,29 +136,47 @@ print 'Average MI(n2;n0) = ' + str(MI_2_0_Avg)
 
 # Produce Graphs----------------------------------------------------------------
 
-#plot raster
+# Plot Mutual Information
+
+#set up x values (g - for conductance)
+g_range  = [a*g_bin for a in range(1,g_res)]
+#use this to plot a line with slope -1 descending from y=1 till y=0
+g_range_ = [1-g for g in g_range]
+
+#perform linear regression on generated data
+y = np.array(MI_2_0)
+x = np.array(g_range)
+A = np.vstack([x, np.ones(len(x))]).T
+m, c = np.linalg.lstsq(A, y)[0]
+
+#scatter Mutual Information between neurons 0 & 1 
+#and plot fitted line
+plt.figure(2)
+plt.plot(x, m*x + c)
+plt.plot(x, x) 
+plt.scatter(x, y)
+plt.ylabel('$Mutual$ $information$ $I(n_2;n_0)$', fontsize=20)
+plt.xlabel('$Synapse$ $strength$ $g_{2,0}$', fontsize=20)
+plt.show()
+
+'''
+plt.figure(3)
+plt.plot(g_range, g_range_)
+plt.scatter(g_range, MI_2_1)
+plt.ylabel('$Mutual$ $information$ $I(n_2;n_1)$', fontsize=20)
+plt.xlabel('$Synapse$ $strength$ $g_{2,1}$', fontsize=20)
+plt.show()
+'''
+
+# Check out latest raster plot
 time = np.arange(0, T, dt)
 
-figure(1)
-ylim([-1, N])
-yticks(np.arange( 0, N, 1))
-xticks(np.arange(0, T, 0.05))
-p3 = plot(time, np.transpose(raster), 'b.')
+plt.figure(1)
+plt.ylim([-1, N])
+plt.yticks(np.arange( 0, N, 1))
+plt.xticks(np.arange(0, T, 0.05))
+p3 = plt.plot(time, np.transpose(raster), 'b.')
 plt.grid()
 plt.ylabel('$Neuron$ $indices$', fontsize=20)
 plt.xlabel('$Spike$ $times$ $[s]$', fontsize=20)
-show()
-
-#plot MI
-g_range = [a*g_bin for a in range(1,g_res)]
-g_range_ = [1-g for g in g_range]
-figure(2)
-plot(g_range, g_range) 
-scatter(g_range, MI_2_0)
-show()
-'''
-figure(3)
-plot(g_range, g_range_)
-scatter(g_range, MI_2_1)
-show()
-'''
+plt.show()